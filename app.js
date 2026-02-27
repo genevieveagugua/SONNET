e38129bfc660b9d2c748b1e94ad3f0fa181ffcad
genevieveagugua <kelechigenevieveagugua@gmail.com>
2026-02-27 15:03:22 -0800

    Add copylink function to JS file

--- app.js
+++ app.js
@@ -20,3 +20,22 @@ document.getElementById("message").textContent = `"${note}"`;
 // Spotify embed
 const embedUrl = song.replace("open.spotify.com/", "open.spotify.com/embed/");
 document.getElementById("spotify-player").src = embedUrl;
+
+// Copy Link
+function copyLink() {
+  const viewPageUrl = `${window.location.origin}/view.html?song-link=${encodeURIComponent(song)}&from-name=${encodeURIComponent(from)}&to-name=${encodeURIComponent(to)}&your-note=${encodeURIComponent(note)}`;
+  const shareBtnText = document.getElementById("share-button");
+
+  navigator.clipboard
+    .writeText(viewPageUrl)
+    .then(() => {
+      shareBtnText.innerHTML = '<i class="fa-solid fa-check"></i>Copied!';
+      setTimeout(() => {
+        shareBtnText.innerHTML = '<i class="fa-regular fa-copy"></i>Copy Link';
+      }, 3000);
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+      alert("Failed to copy link. Please try again.");
+    });
+}
